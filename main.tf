21f73a79f1e387e43798e6ec07a95bd2080fb7d8
TJKapilDevOps <toml29686@gmail.com>
2025-06-22 19:03:24 -0700

    formated main.tf file

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-            ami = var.ami_id
+  ami           = var.ami_id
   instance_type = var.instance_type
-       key_name = var.key_name
+  key_name      = var.key_name
 
   tags = {
     Name = "Terraform-EC2"
